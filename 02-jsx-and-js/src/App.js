61d5831580bd8fab13af052323932fe86456bac9
Gabriel C <gabrielcedran@gmail.com>
2022-02-15 20:07:10 +0000

    Rework vanilla react components into jsx react components

---
+++ 02-jsx-and-js/src/App.js
@@ -0,0 +1,36 @@
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
+
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt Me"),
+//     React.createElement(Pet, {
+//       name: "Luna",
+//       animal: "Dog",
+//       breed: "Havanese",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Pepper",
+//       animal: "Bird",
+//       breed: "Cocktiel",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Sudo",
+//       animal: "Dog",
+//       breed: "Wheaten Terrier",
+//     }),
+//   ]);
+// };
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cocktiel" />
+      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
+    </div>
+  );
+};
+
+ReactDOM.render(App(), document.getElementById("root"));
