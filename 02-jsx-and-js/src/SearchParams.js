05cb5c4d1c9983d546da40312d2d81e658e6cc20
Gabriel C <gabrielcedran@gmail.com>
2022-02-16 20:19:59 +0000

    Create breed and animal components with useState hook

--- 02-jsx-and-js/src/SearchParams.js
+++ 02-jsx-and-js/src/SearchParams.js
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "dog", "cat", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("London, UK");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -14,6 +19,38 @@ const SearchParams = () => {
             onChange={(event) => setLocation(event.target.value)}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(event) => setAnimal(event.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(event) => setBreed(event.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option value={breed} key={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
