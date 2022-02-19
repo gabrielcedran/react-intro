daa748009ae9f511cbf570230591d1e1324b32c0
Gabriel C <gabrielcedran@gmail.com>
2022-02-19 12:44:28 +0000

    Create an error boundary component for error handling - it is only possible with class components

--- 02-jsx-and-js/src/Details.js
+++ 02-jsx-and-js/src/Details.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
   constructor() {
@@ -12,7 +13,6 @@ class Details extends Component {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
     );
-
     const json = await res.json();
     this.setState(
       Object.assign({ loading: false, ...json.pets }, json.pets[0])
@@ -38,4 +38,12 @@ class Details extends Component {
   }
 }
 
-export default withRouter(Details);
+const DetailsWithRouter = withRouter(Details);
+
+export default function DetailsWithErrorBoundary() {
+  return (
+    <ErrorBoundary>
+      <DetailsWithRouter />
+    </ErrorBoundary>
+  );
+}
